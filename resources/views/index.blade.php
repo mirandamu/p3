f12dca9f4bf53d8df63736e21c17caf1d6067631
Miranda Mu <mirandamu@gmail.com>
2016-10-30 18:21:09 -0400

    updating packages

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,5 +2,7 @@
 
 @section('content')
     <h1>Developer's Best Friends</h1>
-    <p>When developing web pages or applications, there is sometimes a need for placeholder text or test user details to get a better picture of how things are coming together. Try using our <a href="lorem-generator/">Lorem Ipsum Generator</a> or our <a href="user-generator/">User Generator</a> to save some time.</p>
+    <p>When developing web pages or applications, there is sometimes a need for placeholder text or test user details to get a better picture of how things are coming together. Try using our <a href="lorem-generator/">Lorem Ipsum Generator</a> or our <a href="user-generator/">User Generator</a> to save some time.</p><br>
+    <img id="newyorker" alt="New Yorker cartoon" src="{{ URL::asset('/images/newyorkercartoon.jpg') }}"/>
+    <p id="picturesource"><a href="http://www.newyorker.com/cartoons/a15605" target="_BLANK">Image source</a></p>
 @endsection
